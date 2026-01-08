4f1ec2f05863f5dcf088d4df554c4a9797251895
Rishav Gairola <101463798+Gairolarishav@users.noreply.github.com>
2026-01-08 19:30:02 +0530

    Add API endpoint for ingesting PDF documents

---
+++ app/api/create_embeddings.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter,UploadFile, File,HTTPException
+from app.services.rag.embeddings import create_embeddings
+
+
+router  = APIRouter(prefix="/embeddings",tags=["Embeddings"])
+
+@router.post("/ingest/")
+async def ingest_documents(
+    role : str,
+    file: UploadFile = File(...)
+):
+    # Validate file type
+    if file.content_type != "application/pdf":
+        raise HTTPException(status_code=400, detail="Only PDF files are allowed")
+
+    # Read file bytes
+    file_bytes = await file.read()
+
+    result  = create_embeddings(role,file_bytes,filename=file.filename)
+
+    return {
+        "filename": file.filename,
+        "message": result
+    }
